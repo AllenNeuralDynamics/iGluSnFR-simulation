ea3e9900e29e2e5d3f07c4983c047d7f7cac17c5
j-friedrich <johannes.friedrich@alleninstitute.org>
2025-02-05 00:50:50 +0000

    change default bleaching time constant to 6 minutes

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -474,7 +474,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--bleachTau",
         type=int,
-        default=10000,
+        default=360,  # JF changed, was 10000
         help="Exponential time constant of bleaching in seconds.",
     )
     parser.add_argument(
